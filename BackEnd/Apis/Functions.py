e0a3718ca473116ce9b4004efc4a65194ef795cc
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-08-31 09:14:01 -0600

    feat: Creación de API UpdatePalabraById y comentarios

--- BackEnd/Apis/Functions.py
+++ BackEnd/Apis/Functions.py
@@ -22,6 +22,13 @@ if Keys.strConnection != None:
     dbConnection = Keys.dbConnection
 
 def fnGetAllPalabras():
+    """
+    Recupera todas las palabras en la base de datos.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Objeto resultante con la información de la base de datos.
+    """
     try:
         cursor = dbConnection.palabras.find({}, PROYECTION)
 
@@ -37,11 +44,11 @@ def fnGetPalabraByNombre(palabra):
     Recupera una palabra según su nombre en la base de datos.
 
     Parámetros:
-    - palabra (str): El nombre de la palabra a buscar.
+    - palabra (string): El nombre de la palabra a buscar.
 
     Returns:
-    - intStatus: Estado de la petición HTTP xitosa.
-    - strAnswer: Respuesta de la petición HTTP exitosa.
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Objeto resultante con la información de la base de datos.
     """
     try:
         if not palabra:
@@ -57,6 +64,17 @@ def fnGetPalabraByNombre(palabra):
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
 def fnPostPalabra(strPalabra, strVideo):
+    """
+    Agrega una palabra y su respectivo video a la base de datos.
+
+    Parámetros:
+    - strPalabra (string): El nombre de la palabra a agregar.
+    - strVideo (string): El video de la palabra a agregar.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Mensaje de información agregada correctamente.
+    """
     try:
         if not strPalabra or not strVideo:
             return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
@@ -67,4 +85,30 @@ def fnPostPalabra(strPalabra, strVideo):
         return {'intStatus': 200, 'strAnswer': "Palabra insertada correctamente"}
     except Exception as error:
         logging.exception("Error en fnPostPalabra: {}".format(error))
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+def fnUpdatePalabraById(objectId, strPalabra, strVideo):
+    """
+    Actualiza una palabra y su respectivo video en la base de datos.
+
+    Parámetros:
+    - objectId (string): El id de la palabra a actualizar.
+    - strPalabra (string): El nombre de la palabra a actualizar.
+    - strVideo (string): El video de la palabra a actualizar.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Mensaje de información actualizada correctamente.
+    """
+    try:
+        if not objectId:
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        jsnQuery = {"_id": ObjectId(objectId)}
+        cursor = {"$set": {"palabra": strPalabra, "video": strVideo}}
+        dbConnection.palabras.update_one(jsnQuery, cursor)
+
+        return {'intStatus': 200, 'strAnswer': "Palabra actualizada correctamente"}
+    except Exception as error:
+        logging.exception("Error en fnUpdatePalabra: {}".format(error))
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
--- BackEnd/Directions.py
+++ BackEnd/Directions.py
@@ -15,8 +15,18 @@ from flask import Flask, jsonify, request, Response
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/general/GetAllPalabras', methods=['GET'])
+@app.route('/api/general/getAllPalabras', methods=['GET'])
 def GetAllPalabras():
+    """
+    Recupera todos los datos de la colección 'palabras' en la base de datos.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnGetAllPalabras.
+
+    Raises:
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
     try:
         objResult = callMethod.fnGetAllPalabras()
 
@@ -28,8 +38,23 @@ def GetAllPalabras():
         logging.exception("Error en GetAllPalabras: {}".format(error))
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
-@app.route('/api/general/GetPalabraByNombre/<string:palabra>', methods=['GET'])
+
+@app.route('/api/general/getPalabraByNombre/<string:palabra>', methods=['GET'])
 def GetPalabraByNombre(palabra):
+    """
+    Recupera una palabra en base a su nombre.
+
+    Parámetros:
+    - palabra (string): El nombre de la palabra a buscar.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnGetPalabraByNombre.
+
+    Raises:
+    - 400 Bad Request: Si el campo 'palabra' no está en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
     try:
         if not palabra:
             return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
@@ -44,14 +69,26 @@ def GetPalabraByNombre(palabra):
         logging.exception("Error en GetPalabraById: {}".format(error))
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
-@app.route('/api/general/PostPalabra', methods=['POST'])
+
+@app.route('/api/general/postPalabra', methods=['POST'])
 def PostPalabra():
+    """
+    Agrega una palabra y un video a la base de datos.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnPostPalabra.
+
+    Raises:
+    - 400 Bad Request: Si los campos 'palabra' o 'video' no están en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
     try:
         json_data = request.json
         palabra = json_data.get('palabra', None)
         video = json_data.get('video', None)
 
-        if not palabra or not video:
+        if not palabra or not video or not isinstance(palabra, str) or not isinstance(video, str):
             return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
 
         objResult = callMethod.fnPostPalabra(palabra, video)
@@ -63,6 +100,42 @@ def PostPalabra():
     except Exception as error:
         logging.exception("Error en PostPalabra: {}".format(error))
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+@app.route('/api/general/updatePalabraById/<string:_id>', methods=['PUT'])
+def updatePalabraById(_id):
+    """
+    Actualiza una palabra en base a su id.
+
+    Parámetros:
+    - _id (string): El id de la palabra a actualizar.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnUpdatePalabraById.
+
+    Raises:
+    - 400 Bad Request: Si los campos 'palabra' o 'video' no están en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
+    try:
+        json_data = request.json
+        palabra = json_data.get('palabra', None)
+        video = json_data.get('video', None)
+
+        if not palabra or not video or not isinstance(_id, str) or not isinstance(palabra, str) or not isinstance(video, str):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        objResult = callMethod.fnUpdatePalabraById(_id, palabra, video)
+
+        if not objResult or not isinstance(objResult, dict):
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return jsonify(objResult)
+    except Exception as error:
+        logging.exception("Error en updatePalabraById: {}".format(error))
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9005, debug=True, threaded=True)
