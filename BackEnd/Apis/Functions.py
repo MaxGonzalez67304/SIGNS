89e782c438ec720d0adaf364c6496c227ba7efd4
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-09-05 18:12:19 -0600

    feat: Creación de APIs de usuarios

--- BackEnd/Apis/Functions.py
+++ BackEnd/Apis/Functions.py
@@ -143,3 +143,125 @@ def fnDeletePalabraById(objectId):
     except Exception as error:
         logging.exception("Error en fnDeletePalabra: %s", error)
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+def fnGetAllUsuarios():
+    """
+    Recupera todos los usuarios en la base de datos.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Objeto resultante con la información de la base de datos.
+    """
+    try:
+        cursor = dbConnection.usuarios.find({}, PROYECTION)
+
+        objResult = [document for document in cursor]
+
+        return {'intStatus': 200, 'strAnswer': objResult}
+    except Exception as error:
+        logging.exception("Error en getAllUsuarios: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+def fnGetUsuarioById(objectId):
+    """
+    Recupera un usuario según su id en la base de datos.
+
+    Parámetros:
+    - _id (string): El id del usuario a buscar.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Objeto resultante con la información de la base de datos.
+    """
+    try:
+        if not ObjectId.is_valid(objectId):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        jsnQuery = {"_id": ObjectId(objectId)}
+        cursor = dbConnection.usuarios.find_one(jsnQuery, PROYECTION)
+        objResult = cursor
+
+        return {'intStatus': http.HTTPStatus.OK, 'strAnswer': objResult}
+    except Exception as error:
+        logging.exception("Error en fnGetUsuarioById: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+def fnPostUsuario(strNombre, strCorreo, strUsername, strPassword):
+    """
+    Agrega un usuario a la base de datos.
+
+    Parámetros:
+    - strNombre (string): El nombre del usuario a agregar.
+    - strCorreo (string): El correo del usuario a agregar.
+    - strUsername (string): El username del usuario a agregar.
+    - strPassword (string): El password del usuario a agregar.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Mensaje de información agregada correctamente.
+    """
+    try:
+        if not strNombre or not strCorreo or not strUsername or not strPassword:
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        jsnQuery = {"nombre": strNombre, "correo": strCorreo, "username": strUsername, "password": strPassword}
+        dbConnection.usuarios.insert_one(jsnQuery)
+
+        return {'intStatus': 200, 'strAnswer': "Usuario insertado correctamente"}
+    except Exception as error:
+        logging.exception("Error en fnPostUsuario: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+def fnUpdateUsuarioById(objectId, strNombre, strCorreo, strUsername, strPassword):
+    """
+    Actualiza un usuario en la base de datos.
+
+    Parámetros:
+    - objectId (string): El id del usuario a actualizar.
+    - strNombre (string): El nombre del usuario a actualizar.
+    - strCorreo (string): El correo del usuario a actualizar.
+    - strUsername (string): El username del usuario a actualizar.
+    - strPassword (string): El password del usuario a actualizar.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Mensaje de información actualizada correctamente.
+    """
+    try:
+        if not ObjectId.is_valid(objectId):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        jsnQuery = {"_id": ObjectId(objectId)}
+        cursor = {"$set": {"nombre": strNombre, "correo": strCorreo, "username": strUsername, "password": strPassword}}
+        dbConnection.usuarios.update_one(jsnQuery, cursor)
+
+        return {'intStatus': 200, 'strAnswer': "Usuario actualizado correctamente"}
+    except Exception as error:
+        logging.exception("Error en fnUpdateUsuario: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+def fnDeleteUsuarioById(objectId):
+    """
+    Elimina un usuario en la base de datos.
+
+    Parámetros:
+    - objectId (string): El id del usuario a eliminar.
+
+    Returns:
+    - intStatus: Estado de la petición HTTP exitosa.
+    - strAnswer: Mensaje de información eliminada correctamente.
+    """
+    try:
+        if not ObjectId.is_valid(objectId):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        jsnQuery = {"_id": ObjectId(objectId)}
+        cursor = dbConnection.usuarios.delete_one(jsnQuery)
+
+        if cursor.deleted_count == 0 or not cursor:
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return {'intStatus': 200, 'strAnswer': "Usuario eliminado correctamente"}
+    except Exception as error:
+        logging.exception("Error en fnDeleteUsuario: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
--- BackEnd/Directions.py
+++ BackEnd/Directions.py
@@ -168,6 +168,157 @@ def deletePalabraById(_id) -> jsonify:
         logging.exception("Error en deletePalabraById: %s", error)
         return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
+@app.route('/api/general/getAllUsuarios', methods=['GET'])
+def GetAllUsuarios() -> jsonify:
+    """
+    Recupera todos los datos de la colección 'usuarios' en la base de datos.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnGetAllUsuarios.
+
+    Raises:
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
+    try:
+        objResult = callMethod.fnGetAllUsuarios()
+
+        if not objResult or not isinstance(objResult, dict):
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return jsonify(objResult)
+    except Exception as error:
+        logging.exception("Error en GetAllUsuarios: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+@app.route('/api/general/getUsuarioById/<string:_id>', methods=['GET'])
+def GetUsuarioById(_id) -> jsonify:
+    """
+    Recupera un usuario en base a su id.
+
+    Parámetros:
+    - _id (string): El id del usuario a buscar.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnGetUsuarioById.
+
+    Raises:
+    - 400 Bad Request: Si el campo '_id' no está en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
+    try:
+        if not _id or not isinstance(_id, str):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        objResult = callMethod.fnGetUsuarioById(_id)
+
+        if not objResult or not isinstance(objResult, dict):
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return jsonify(objResult)
+    except Exception as error:
+        logging.exception("Error en GetUsuarioById: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+@app.route('/api/general/postUsuario', methods=['POST'])
+def PostUsuario() -> jsonify:
+    """
+    Agrega un usuario a la base de datos.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnPostUsuario.
+
+    Raises:
+    - 400 Bad Request: Si los campos 'nombre', 'username', 'correo' o 'password' no están en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
+    try:
+        json_data = request.json
+        nombre = json_data.get('nombre', None)
+        correo = json_data.get('correo', None)
+        username = json_data.get('username', None)
+        password = json_data.get('password', None)
+
+        if not nombre or not username or not correo or not password or not isinstance(nombre, str) or not isinstance(username, str) or not isinstance(correo, str) or not isinstance(password, str):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        objResult = callMethod.fnPostUsuario(nombre, username, correo, password)
+
+        if not objResult or not isinstance(objResult, dict):
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return jsonify(objResult)
+    except Exception as error:
+        logging.exception("Error en PostUsuario: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+@app.route('/api/general/updateUsuarioById/<string:_id>', methods=['PUT'])
+def updateUsuarioById(_id) -> jsonify:
+    """
+    Actualiza un usuario en base a su id.
+
+    Parámetros:
+    - _id (string): El id del usuario a actualizar.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnUpdateUsuarioById.
+
+    Raises:
+    - 400 Bad Request: Si los campos 'nombre', 'username', 'correo' o 'password' no están en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a actualizar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
+    try:
+        json_data = request.json
+        nombre = json_data.get('nombre', None)
+        correo = json_data.get('correo', None)
+        username = json_data.get('username', None)
+        password = json_data.get('password', None)
+
+        if not nombre or not username or not correo or not password or not isinstance(_id, str) or not isinstance(nombre, str) or not isinstance(username, str) or not isinstance(correo, str) or not isinstance(password, str):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        objResult = callMethod.fnUpdateUsuarioById(_id, nombre, correo, username, password)
+
+        if not objResult or not isinstance(objResult, dict):
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return jsonify(objResult)
+    except Exception as error:
+        logging.exception("Error en updateUsuarioById: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+@app.route('/api/general/deleteUsuarioById/<string:_id>', methods=['DELETE'])
+def deleteUsuarioById(_id) -> jsonify:
+    """
+    Elimina un usuario en base a su id.
+
+    Parámetros:
+    - _id (string): El id del usuario a eliminar.
+
+    Returns:
+    - objResult: El objeto JSON recibido de fnDeleteUsuarioById.
+
+    Raises:
+    - 400 Bad Request: Si el campo '_id' no está en la petición JSON.
+    - 404 Not Found: Si no se encontró el id de la palabra a eliminar.
+    - 500 Internal Server Error: Si ocurre un error en el servidor.
+    """
+    try:
+        if not _id or not isinstance(_id, str):
+            return jsonify(ResponseMessages.err400), http.HTTPStatus.BAD_REQUEST
+
+        objResult = callMethod.fnDeleteUsuarioById(_id)
+
+        if not objResult or not isinstance(objResult, dict):
+            return jsonify(ResponseMessages.err404), http.HTTPStatus.NOT_FOUND
+
+        return jsonify(objResult)
+    except Exception as error:
+        logging.exception("Error en deleteUsuarioById: %s", error)
+        return jsonify(ResponseMessages.err500), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9005, debug=True, threaded=True)
